c01d422093b5f415823fd23ad37f13b32a22a1b7
carlos <carlosberdejopar@gmail.com>
2024-12-04 17:28:49 -0500

    añadir filtro

--- js/carta-target.js
+++ js/carta-target.js
@@ -1,4 +1,130 @@
-function cartas() {
+
+class Target{
+    idcount = 0;
+    /**
+     * 
+     * @param {String} imagen 
+     * @param {String} video 
+     * @param {String} titulo 
+     * @param {} precio 
+     */
+    constructor( imagen, video, titulo, precio,filtro = Filtros.Todos,categoria) {
+        this.id = this.idcount++;
+        this.imagen = imagen;
+        this.video = video;
+        this.titulo = titulo;
+        this.precio = precio;
+        this.filtro = filtro;
+        this.categoria = categoria;
+    }
+}
+const Categoria = {
+    Pollo: 'Pollo',
+    Carnes: 'Carnes',
+    Pescado: 'Pescado',
+    Pastas: 'Pastas',
+    Tragos:'Tragos'
+}
+const Filtros = {
+    Todos: 'Todos',
+    Tendencia: 'Tendencia',
+    Baratos: 'Baratos',
+    Mejores:'Mejores',
+}
+const {Carnes,Pastas,Pollo,Pescado,Tragos} = Categoria;
+const menu = {
+    todos: [
+        new Target('IMAGENES/pla1.jpg', 'IMAGENES/vi1.mp4', 'Seca de Carne', 'S/.20',Filtros.Mejores,Carnes),
+        new Target('IMAGENES/pla2.jpg','IMAGENES/huancaina.mp4', 'Arroz Chaufa', 'S/.25',Filtros.Tendencia,Carnes),
+        new Target('IMAGENES/pla3.jpg', 'IMAGENES/vi1.mp4', 'Tallarin', 'S/.30'),
+        new Target('IMAGENES/pla4.jpg', 'IMAGENES/huancaina.mp4', 'Tallarin Saltado', 'S/.35'),
+    ],
+    filtro: Filtros.Todos
+
+}
+//Stores
+const getFiltros = () => {
+    return menu.filtro;
+}
+const getMenusFiltros = ( filter = Filtros.Todos ) => {
+    switch (filter) {
+        case Filtros.Todos:
+            return [...menu.todos];
+        case Filtros.Baratos:
+            return menu.todos.filter(plato => plato.filtro === Filtros.Baratos);
+        case Filtros.Mejores:
+            return menu.todos.filter(plato => plato.filtro === Filtros.Mejores);
+        case Filtros.Tendencia:
+            return menu.todos.filter(plato => plato.filtro === Filtros.Tendencia);
+    }
+}
+
+const setFiltros = ( newfilter = Filtros.Todos ) => {
+    menu.filtro = newfilter;
+}
+
+//Use-Cases
+const createTarget = ( plato ) => {
+    if (!plato) throw new Error('Se requiere el plato');
+    const html = `  <div class="lista__item">
+                        <a href="Chaufa.html"><img src=${plato.imagen} alt=""></a> 
+                        <a href="Chaufa.html"><video autoplay muted loop src=${plato.video} loop></video></a> 
+                        <div class="fondo__item"></div>
+                        <div class="item__text">
+                            <h3>${plato.titulo}</h3>
+                            <p>Precio: <span>${plato.precio}</span></p>
+                        </div>
+                        <div class="button"><span class="material-symbols-outlined">add</span></div>
+                    </div>`;
+    const divElement = document.createElement('div');
+    divElement.innerHTML = html;
+    divElement.classList.add('lista')
+    return divElement;
+}
+
+const renderTarget = (elementID, menu = []) => {
+    
+    let contenedorLista = document.querySelector(elementID); //'.contenedor__lista')
+    contenedorLista.innerHTML = '';
+    menu.forEach(plato => {
+        contenedorLista.append(createTarget(plato));
+    })
+}
+
+const displayMenu = () => {
+    const Menus = getMenusFiltros(getFiltros());
+    renderTarget('.contenedor__lista', Menus);
+}
+const ElementsHTML = {
+    ButonFilter: '#Filtro',
+};
+    
+(() => {
+    displayMenu();
+    const ButonFiltersMenu = document.querySelectorAll(ElementsHTML.ButonFilter);
+    ButonFiltersMenu.forEach(menus => {
+        menus.addEventListener('click', (event) => {
+            console.log(event.target.text);
+            switch (event.target.text) {
+                case Filtros.Todos:
+                    setFiltros(Filtros.Todos)
+                    break;
+                case Filtros.Baratos:
+                    setFiltros(Filtros.Baratos)
+                    break;
+                case Filtros.Mejores:
+                    setFiltros(Filtros.Mejores)
+                    break;
+                case Filtros.Tendencia:
+                    setFiltros(Filtros.Tendencia)
+                    break;
+            }
+            displayMenu();
+        })
+    })
+})();
+
+/*function cartas() {
     const infoTarget = [
         { id:0, imagen: 'IMAGENES/pla1.jpg',video:'IMAGENES/vi1.mp4', titul: 'Seca de Carne', precio: 'S/.20' },
         { id:1, imagen: 'IMAGENES/pla2.jpg',video:'IMAGENES/vi1.mp4', titul: 'Arroz Chaufa', precio: 'S/.20' },
@@ -43,4 +169,4 @@ function logger(i) {
     selectedCardIds.push(i);
     localStorage.setItem('selectedCardIds', JSON.stringify(selectedCardIds));
     //window.location.href = '../Reservas.html';
-}
+}*/
